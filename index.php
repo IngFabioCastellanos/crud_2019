d8845532904aac69857b973b4674dd5b0a1e4ce2
FabioxUchiha <fcastellanos43@uan.edu.co>
2020-08-06 19:42:31 -0500

    crud terminado

--- index.php
+++ index.php
@@ -9,119 +9,119 @@ $resultado->execute();
 $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crud</title>
-
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="#" />  
+    <title>Tutorial DataTables</title>
+      
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="main.css">
-
-    <link rel="stylesheet" type="text/css" href="datatables/datatables.min.css">
-    <link rel="stylesheet" href="datatables/DataTables-1.10.21/css/dataTables.bootstrap4.min.css">
-
-</head>
-<body>
-    <header>
-        <h3 class="text-center text-light">Tutorial</h3>
-        <h4 class="text-center text-light">Crud con <span class="badge badge-danger">DATATABLES</span></h4>
-    </header>
+    <!-- CSS personalizado --> 
+    <link rel="stylesheet" href="main.css">  
+      
+      
+    <!--datables CSS básico-->
+    <link rel="stylesheet" type="text/css" href="datatables/datatables.min.css"/>
+    <!--datables estilo bootstrap 4 CSS-->  
+    <link rel="stylesheet"  type="text/css" href="datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css">       
+  </head>
     
-    <!--contenedor con el boton Nuevo-->
+  <body> 
+     <header>
+<!--         <h3 class="text-center text-light">Tutorial</h3>-->
+         <h4 class="text-center text-light">CRUD con <span class="badge badge-danger">DATATABLES</span></h4> 
+     </header>    
+      
     <div class="container">
         <div class="row">
-            <div class="col-lg-12">
-                <button id="btnNuevo" type="button" class="btn btn-success">Nuevo</button>
-            </div>
-        </div>
-    </div>
-    <!--fin del contenedor del boton nuevo-->
-
-    <!--contenedor con la tabla personas-->
+            <div class="col-lg-12">            
+            <button id="btnNuevo" type="button" class="btn btn-success" data-toggle="modal">Nuevo</button>    
+            </div>    
+        </div>    
+    </div>    
+    <br>  
     <div class="container">
         <div class="row">
-            <div class="col-lg-12">
-                <div class="table-responsive">
-                    <table id="tablaPersonas" class="table table-striped table-bordered table-condensed" 
-                    style="width: 100%;">
+                <div class="col-lg-12">
+                    <div class="table-responsive">        
+                        <table id="tablaPersonas" class="table table-striped table-bordered table-condensed" style="width:100%">
                         <thead class="text-center">
                             <tr>
                                 <th>Id</th>
                                 <th>Nombre</th>
-                                <th>Pais</th>
-                                <th>Edad</th>
+                                <th>País</th>                                
+                                <th>Edad</th>  
                                 <th>Acciones</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <?php
-                            foreach ($data as $dat) {
-                                ?>
-                                <tr>
-                                    <td><?php echo $dat['id'] ?></td>
-                                    <td><?php echo $dat['nombre'] ?></td>
-                                    <td><?php echo $dat['pais'] ?></td>
-                                    <td><?php echo $dat['edad'] ?></td>
-                                    <td></td>
-                                </tr>
-                                <?php
-                            }
+                            <?php                            
+                            foreach($data as $dat) {                                                        
                             ?>
-                        </tbody>
-                    </table>
+                            <tr>
+                                <td><?php echo $dat['id'] ?></td>
+                                <td><?php echo $dat['nombre'] ?></td>
+                                <td><?php echo $dat['pais'] ?></td>
+                                <td><?php echo $dat['edad'] ?></td>    
+                                <td></td>
+                            </tr>
+                            <?php
+                                }
+                            ?>                                
+                        </tbody>        
+                       </table>                    
+                    </div>
                 </div>
+        </div>  
+    </div>    
+      
+<!--Modal para CRUD-->
+<div class="modal fade" id="modalCRUD" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                </button>
             </div>
+        <form id="formPersonas">    
+            <div class="modal-body">
+                <div class="form-group">
+                <label for="nombre" class="col-form-label">Nombre:</label>
+                <input type="text" class="form-control" id="nombre">
+                </div>
+                <div class="form-group">
+                <label for="pais" class="col-form-label">País:</label>
+                <input type="text" class="form-control" id="pais">
+                </div>                
+                <div class="form-group">
+                <label for="edad" class="col-form-label">Edad:</label>
+                <input type="number" class="form-control" id="edad">
+                </div>            
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-dismiss="modal">Cancelar</button>
+                <button type="submit" id="btnGuardar" class="btn btn-dark">Guardar</button>
+            </div>
+        </form>    
         </div>
     </div>
-    <!--fin del contenedor de la tabla personas-->
-
-    <!--modal para el crud-->
-    <div class="modal fade" id="modalCRUD" tabindex="-1" role="dialog" aria-labelledby="exampModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                    <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel"></h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                        </div>
-                        <form id="formPersonas">
-                            <div class="modal-body">
-                                <div class="form-group">
-                                    <label for="nombre" class="col-form-label">Nombre:</label>
-                                    <input type="text" id="nombre" class="form-control">
-                                </div>
-                                <div class="form-group">
-                                    <label for="pais" class="col-form-label">Pais:</label>
-                                    <input type="text" id="pais" class="form-control">
-                                    </div>
-                                <div class="form-group">
-                                    <label for="edad" class="col-form-label">Edad:</label>
-                                    <input type="number" id="edad" class="form-control">
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-light" data-dismiss="modal">Cancelar</button>
-                                <button type="submit" id="btnGuardar" class="btn btn-dark">Guardar</button>
-                            </div>
-                        </form>
-            </div>
-        </div>
-    </div>
-    <!--fin del modal para el crud-->
-
-
-
-    <!--jQuery, popper.js, Bootstrap Js -->
+</div>  
+      
+    <!-- jQuery, Popper.js, Bootstrap JS -->
     <script src="jquery/jquery-3.3.1.min.js"></script>
-    <script src="poppers/popper.min.js"></script>
+    <script src="popper/popper.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
-
-    <!--DATATABLES JS  -->
-    <script type="text/javascript" src="datatables/datatables.min.js"></script>
-    <script type="text/javascript" src="main.js"></script>
-
-</body>
+      
+    <!-- datatables JS -->
+    <script type="text/javascript" src="datatables/datatables.min.js"></script>    
+     
+    <script type="text/javascript" src="main.js"></script>  
+    
+    
+  </body>
 </html>
